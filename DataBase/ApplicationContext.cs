6de17225d9a31be417fde774042470c8f2768bc0
KAM92W <KAM.92W@yandex.ru>
2024-10-15 10:59:54 +0300

    15.10.24-10:59

--- DataBase/ApplicationContext.cs
+++ DataBase/ApplicationContext.cs
@@ -5,16 +5,16 @@ namespace DataBase
 {
     public class ApplicationContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+        public ApplicationContext() => Database.EnsureCreated();
+
         public DbSet<Brand> Brands => Set<Brand>();
         public DbSet<Category> Categories => Set<Category>();
         public DbSet<Model> Models => Set<Model>();
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Property> Properties => Set<Property>();
-        public ApplicationContext() => Database.EnsureCreated();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=helloapp.db");
-        }
     }
 }
