00cf160cc798e02bb90f9a02953af9d4ab4d7bfe
KAM92W <KAM.92W@yandex.ru>
2024-10-21 16:22:41 +0300

    21.10.24-16:22

--- WebAPI/Controllers/ProductController.cs
+++ WebAPI/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using WebAPI.Dto;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace WebAPI.Controllers;
 
@@ -24,11 +25,23 @@ public class ProductController : ControllerBase
     [HttpPost]
     public void Post(ProductName product)
     {
+        var folderName = Path.Combine("wwwroot", "images");
+        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+        var fileName = product.File.FileName;
+        var fullPath = Path.Combine(pathToSave, fileName);
+        var dbPath = Path.Combine("images", fileName);
+
+        using (var stream = new FileStream(fullPath, FileMode.Create))
+        {
+            product.File.CopyTo(stream);
+        }
+
         using (var db = new ApplicationContext())
         {
             var entity = new Product
             {
-                Picture = "Should be the link to image here",
+                Picture = dbPath,
                 BrandId = product.BrandId,
                 CategoryId = product.CategoryId,
                 ModelId = product.ModelId,
@@ -40,30 +53,55 @@ public class ProductController : ControllerBase
         }
     }
 
-    [HttpPost("UpLoad"), DisableRequestSizeLimit]
-    public async Task<IActionResult> UploadFile([FromForm] ProductName model)
-    {
-        //if (model.File == null && model.File.Length == 0)
-        //{
-        //    return BadRequest("Invalid file");
-        //}
-        var folderName = Path.Combine("wwwroot", "images");
-        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-        //if (!Directory.Exists(pathToSave))
-        //{
-        //    Directory.CreateDirectory(pathToSave);
-        //}
-        var fileName = model.File.FileName;
-        var fullPath = Path.Combine(pathToSave, fileName);
-        var dbPath = Path.Combine(folderName, fileName);
-        //if (System.IO.File.Exists(fullPath))
-        //{
-        //    return BadRequest("file already exist");
-        //}
-        using (var stream = new FileStream(fullPath, FileMode.Create))
-        {
-            model.File.CopyTo(stream);
-        }
-        return Ok(new { dbPath });
-    }
+    //[HttpPost("UpLoad"), DisableRequestSizeLimit]
+    //public async Task<IActionResult> UploadFile([FromForm] ProductName model)
+    //{
+    //    return Ok();
+    //}
+
+    //[HttpPost]
+    //public void Post(ProductName product)
+    //{
+    //    using (var db = new ApplicationContext())
+    //    {
+    //        var entity = new Product
+    //        {
+    //            Picture = "Should be the link to image here",
+    //            BrandId = product.BrandId,
+    //            CategoryId = product.CategoryId,
+    //            ModelId = product.ModelId,
+    //            PriceId = product.PriceId,
+    //            Properties = []
+    //        };
+    //        db.Products.Add(entity);
+    //        db.SaveChanges();
+    //    }
+    //}
+
+    //    [HttpPost("UpLoad"), DisableRequestSizeLimit]
+    //    public async Task<IActionResult> UploadFile([FromForm] ProductName model)
+    //    {
+    //        //if (model.File == null && model.File.Length == 0)
+    //        //{
+    //        //    return BadRequest("Invalid file");
+    //        //}
+    //        var folderName = Path.Combine("wwwroot", "images");
+    //        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+    //        //if (!Directory.Exists(pathToSave))
+    //        //{
+    //        //    Directory.CreateDirectory(pathToSave);
+    //        //}
+    //        var fileName = model.File.FileName;
+    //        var fullPath = Path.Combine(pathToSave, fileName);
+    //        var dbPath = Path.Combine(folderName, fileName);
+    //        //if (System.IO.File.Exists(fullPath))
+    //        //{
+    //        //    return BadRequest("file already exist");
+    //        //}
+    //        using (var stream = new FileStream(fullPath, FileMode.Create))
+    //        {
+    //            model.File.CopyTo(stream);
+    //        }
+    //        return Ok(new { dbPath });
+    //    }
 }
